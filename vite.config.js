6db04666eabcab59f9689fd9c078e575303379ee
Poonam FastFurious <your-email@example.com>
2024-07-02 18:42:12 +0530

    update vite

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-=======
-  server: {
-    proxy: {
-      "/api": "https://ssagricultureapi.brandbell.in",
-    },
-  },
->>>>>>> c727a857f65edd70d1312c0e9c24c090f0e0b5eb
   plugins: [react()],
 });
